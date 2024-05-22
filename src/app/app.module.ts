8298a842abfa2367c6b0e789347b3180aa0b3251
Makame Juma <makame.juma@forest-interactive.com>
2024-05-22 17:46:39 +0800

    Removed location strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,9 +19,11 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [{
-    provide:LocationStrategy,useClass:HashLocationStrategy
-  }],
+  providers: [
+  //   {
+  //   provide:LocationStrategy,useClass:HashLocationStrategy
+  // }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
